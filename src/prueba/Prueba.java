f9edd69ed5f8fab7212cbc8f25a9961e4d62f945
leo283 <germanclarinet03@gmail.com>
2022-02-03 22:41:58 -0500

    prueba de push

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,5 +3,6 @@ package prueba;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Esta es la segunda prueba! :)");
     }
 }
