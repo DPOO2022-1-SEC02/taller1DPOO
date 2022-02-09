b7e7c00f32844ed477da1e285495c1bd4d45c7d9
leo283 <germanclarinet03@gmail.com>
2022-02-09 09:22:49 -0500

    Prueba

--- src/consola/Aplicacion.java
+++ src/consola/Aplicacion.java
@@ -2,11 +2,16 @@ package consola;
 
 import modelo.Restaurante;
 
+import java.util.Scanner;
+
 public class Aplicacion {
     private Restaurante restaurante;
+    private Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
         String hola="%hola";
         hola=hola.replace("%","");
+
+
     }
 
 }
